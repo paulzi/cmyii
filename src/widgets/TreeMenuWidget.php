3fd1d0fc2d9d93cc875f37f94e3658a39cfed3ae
PaulZi <pavel.zimakoff@gmail.com>
2018-05-31 01:06:40 +0300

    roles for TreeMenuWidget

--- src/widgets/TreeMenuWidget.php
+++ src/widgets/TreeMenuWidget.php
@@ -3,8 +3,9 @@
 namespace paulzi\cmyii\admin\widgets;
 
 use Yii;
-use paulzi\cmyii\admin\components\TreeMenuSources;
 use yii\base\Widget;
+use paulzi\cmyii\Cmyii;
+use paulzi\cmyii\admin\components\TreeMenuSources;
 use paulzi\cmyii\admin\CmyiiAdmin;
 
 class TreeMenuWidget extends Widget
@@ -36,18 +37,23 @@ class TreeMenuWidget extends Widget
     {
         $method = 'treeMenu' . ucfirst($this->type);
         if (isset(CmyiiAdmin::getInstance()->menuSources[$this->type])) {
-            $data = call_user_func(CmyiiAdmin::getInstance()->menuSources[$this->type], $this->id);
+            $list = call_user_func(CmyiiAdmin::getInstance()->menuSources[$this->type], $this->id);
         } elseif(method_exists(TreeMenuSources::className(), $method)) {
-            $data = TreeMenuSources::$method($this->id);
+            $list = TreeMenuSources::$method($this->id);
         } else {
-            $data = [];
+            $list = [];
         }
-        foreach ($data as &$item) {
+        $data = [];
+        foreach ($list as $item) {
             if (is_string($item)) {
                 $item = CmyiiAdmin::getInstance()->menuAlias[$item];
             }
+            $roles = isset($item['roles']) ? $item['roles'] : '';
+            $roles = is_array($roles) ? implode(',', $roles) : $roles;
+            if (Cmyii::checkRoles($roles, ['item' => $item])) {
+                $data[] = $item;
+            }
         }
-        unset($item);
         if ($this->expanded === null && $this->cookie !== null) {
             $list = isset($_COOKIE[$this->cookie]) ? $_COOKIE[$this->cookie] : null;
             $list = array_flip(explode(',', $list));
